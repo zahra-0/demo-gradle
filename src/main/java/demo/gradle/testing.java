7ff31d307b0be165263235e2e344f45d2cd289db
zahra-0 <95011503+zahra-0@users.noreply.github.com>
2021-11-29 19:41:47 +0530

    first commit on gradle project

---
+++ src/main/java/demo/gradle/testing.java
@@ -0,0 +1,11 @@
+package demo.gradle;
+
+public class testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Snowy & Mayo Mayo from Java class");
+
+	}
+
+}
